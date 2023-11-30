899ab81f886360d53cc9f29b80e3ae574d4dc9b9
Zahirul <jislam150@gmail.com>
2023-11-30 08:01:32 +0600

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Hologram1 on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
